0ae6b2ab388c7cebde27e9fe2031cbde22ad022d
Winnie <wimacoj@gmail.com>
2024-02-20 19:01:09 +0300

    Resbumitting corrections

--- lib/Author.py
+++ lib/Author.py
@@ -1,8 +1,33 @@
+from Article import Article
+from Magazine import Magazine
+
 class Author:
+    _all_authors = []  # List to store all Author instances
+
     def __init__(self, name):
         # Initializes an Author with a given name.
         self._name = name
+        self._articles = []  # List to store Article instances by this author
+        Author._all_authors.append(self)
 
     def name(self):
         # Returns the name of the author.
         return self._name
+
+    def add_article(self, magazine, title):
+        # creates a new Article instance and associates it with that author and that magazine.
+        new_article = Article(self, magazine, title)
+        self._articles.append(new_article)
+
+    def articles(self):
+        # Returns a list of Article instances written by the author.
+        return self._articles
+
+    def magazines(self):
+        # Returns a unique list of Magazine instances for which the author has contributed.
+        return list(set(article.magazine() for article in self._articles))
+
+    @classmethod
+    def all(cls):
+        # Returns a list of all Author instances.
+        return cls._all_authors
--- lib/Magazine.py
+++ lib/Magazine.py
@@ -8,7 +8,7 @@ class Magazine:
         self._contributors = []  # List to store Author instances who have written for this magazine
         Magazine._all_magazines.append(self)
 
-    def get_name(self):
+    def find_by_name(self):
         # Returns the name of the magazine.
         return self._name
 
@@ -16,7 +16,17 @@ class Magazine:
         # Returns the category of the magazine.
         return self._category
 
+    def contributing_authors(self, author):
+        # Adds an Author instance to the list of contributors for this magazine.
+        if author not in self._contributors:
+            self._contributors.append(author)
+
+    def contributors(self):
+        # Returns a list of Author instances who have written for this magazine.
+        return self._contributors
+
     @classmethod
     def all(cls):
         # Returns a list of all Magazine instances.
         return cls._all_magazines
+
